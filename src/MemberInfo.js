5ffe9c3023981cbcf01639f283aa633dfc8680a5
Emily Brick <emilybrick@github.com>
2018-10-09 10:58:54 -0400

    removed line and changed the question blue color

--- src/MemberInfo.js
+++ src/MemberInfo.js
@@ -12,7 +12,7 @@ injectGlobal`
 
 const MemberInfo = ({member}) => (
   <Box maxWidth="600px" id={member.handle}>
-    <Text is="p" fontSize={4} mb={0} color="blue.4">
+    <Text is="p" fontSize={4} mb={0} mt={0} color="blue.4">
       <Text fontWeight="bold">{member.name}</Text>, {member.title}
     </Text>
     <FlexContainer alignItems="center" mt={2}>
@@ -26,27 +26,27 @@ const MemberInfo = ({member}) => (
         <Octicon size="24" color="blue.3" className="mr-3" icon={MarkGithub} />@{member.handle}
       </Link>
     </FlexContainer>
-    <Box mt={4}>
-      <Text fontWeight="bold" mr={1} fontSize={3}>
+    <Box mt={5}>
+      <Text color="blue.3" fontWeight="bold" mr={1} fontSize={3} is="p">
         Favorite CSS feature:
       </Text>
       <Text fontSize="3" fontFamily="mono">
         {member.cssFeature}
       </Text>
     </Box>
-    <Text fontWeight="bold" is="p" fontSize={3} mt={4}>
+    <Text color="blue.3" fontWeight="bold" is="p" fontSize={3} mt={4}>
       What drew you into design systems?
     </Text>
     <Text fontSize={3}>
       <ReactMarkdown className="markdown" source={member.questionOne} />
     </Text>
-    <Text fontWeight="bold" is="p" fontSize={3} mt={4}>
+    <Text color="blue.3" fontWeight="bold" is="p" fontSize={3} mt={4}>
       Who have you learned from or been inspired by?
     </Text>
     <Text fontSize={3}>
       <ReactMarkdown className="markdown" source={member.questionTwo} />
     </Text>
-    <Text fontWeight="bold" mr={1} fontSize={3} mt={4}>
+    <Text color="blue.3" fontWeight="bold" mr={1} fontSize={3} mt={4}>
       Favorite tools:
     </Text>
     <Text fontSize={3}>
